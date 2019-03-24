9a7f8b85e985c527a81f1475fa60bf5fbe59aa38
Dawid Galeziewski <dawidgalsino@vp.pl>
2019-03-24 21:38:05 +0100

    Basic tools overview

---
+++ notes.js
@@ -0,0 +1,7 @@
+// Nice extensions to have:
+// #1 Emmet
+// #2 Live server - no need to refresh the file
+    // to open with liveserver in visual code right click on the ide and select open with live server
+
+// #3 bracket pair colorizer - diffrent color for brackets on other nested level
+// #4 javascript code snippets
