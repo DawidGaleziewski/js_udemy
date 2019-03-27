39d3b679daef41f0cba11d42405d66d28889c4a1
Dawid Galeziewski <dawidgalsino@vp.pl>
2019-03-27 09:41:56 +0100

    started lesson on Math object in JS

--- 1_2-basic-concepts/app.js
+++ 1_2-basic-concepts/app.js
@@ -297,6 +297,52 @@ console.clear();
             const sum = val1 + val2;
             console.log(sum); // Gives 56
             console.log(typeof sum);
+console.clear();
+
+// ##Numbers and math object
+        const num1 = 100;
+        const num2 = 50;
+        let valNew;
+
+// Simple math
+    // simple stuff
+    valNew = num1 + num2;
+    valNew = num1 * num2;
+    valNew = num1 / num2;
+
+    // Modulus operator - gives back what remains
+    valNew = 5 % 2; //There are 2x2 and 1 is leftower
+        console.log(valNew) //Gives 1
+
+// Math object and its uses
+            // Math has properties and methods
+    valNew = Math.PI;
+        console.log(valNew);
+
+    // Rounding numbers
+    valNew = Math.round(2.7);
+        console.log(valNew);
+
+    // Rounding numbers - specify if its up or down
+    valNew = Math.ceil(2.4);
+        console.log(valNew);
+    valNew= Math.floor(2.9);
+        console.log(valNew);
+
+    // square of a number
+    valNew= Math.sqrt(64);
+        console.log(valNew);
+
+    // power
+    valNew = Math.pow(8, 2);
+        console.log(valNew);
+
+    // return minimum/maximim number
+    valNew = Math.max(1,2,3,5,7,1,0,234);
+        console.log(valNew);
+
+
+
  
 
 
