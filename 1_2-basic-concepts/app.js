660bc90c770c313646f587c84205a748b4a2e159
Dawid Galeziewski <dawidgalsino@vp.pl>
2019-04-10 22:43:46 +0200

    learned about template literals

--- 1_2-basic-concepts/app.js
+++ 1_2-basic-concepts/app.js
@@ -418,9 +418,60 @@ let array = testConcat.indexOf('L');
         console.log(array);
 
     // Seperate a string into array basing on a character
+            // it is usefull for separating basing on spaces
     array = testConcat.split('I');
     console.log(array);
 
+            // use case
+            const tags ="web development, design, hot, color theory"
+            console.log(tags);
+            console.log(tags.split(","))
 
+    // Replace characters or strings
+            // seems to be replacing only first match
+    const words = "Pawełek Mariuszek Dariuszek"
+            console.log(words.replace("ek", "jjit"));
+
+    // Check if something is in the string returning boolean
+    console.log(words.includes("Paweł"));
+    console.clear();
+
+
+    // ####################Template literals (also called templates literals)
+
+    // when we want to inject something to html using js
+    const literalName = "John";
+    const literalAge = 30;
+    const literalJob = "Web developer";
+    const literalCity = "Miami";
+    let html; 
+
+        //Without template strings (es5)
+            // html string
+            //  if we want to go to diffrent lines we need to concatenate with + signs
+            html= '<ul>' +
+                '<li>Name:' + literalName + '</li>' +
+                '<li>Age:' + literalAge + '</li>' + 
+                '<li>Job:' + literalJob +'</li>' +
+                '<li>City:' + literalCity + '</li>' +
+            '</ul>'
+
+            // injecting the html into the dom
+            document.body.innerHTML = html;
+
+        // Template strings (es6)
+            // template strings makes this easier
+            // it uses backtick, which is to the left of the 1 key on keybord
+            // variables are inside ${variable}
+        let htmlEs6 = `
+            <ul>
+                <li>Name: ${literalName} </li>
+                <li>Age: ${literalAge} </li>
+                <li>Job: ${literalJob} </li>
+                <li>City: ${literalCity}</li>
+                <li>${2 + 2}</li>
+            </ul>
+        `;
+          document.body.innerHTML = htmlEs6;
 
 
