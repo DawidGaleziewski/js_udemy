85d544caf11971878e6f77b5bd8149ab8dcffcce
Dawid Galeziewski <dawidgalsino@vp.pl>
2019-04-11 22:04:01 +0200

    finished literals and started basic array operations

--- 1_2-basic-concepts/app.js
+++ 1_2-basic-concepts/app.js
@@ -463,6 +463,16 @@ let array = testConcat.indexOf('L');
             // template strings makes this easier
             // it uses backtick, which is to the left of the 1 key on keybord
             // variables are inside ${variable}
+
+            // we can include function calls inside the template literals
+            function hello(){
+                return 'hello'
+            }
+
+            // You can also do:
+                // conditionals
+                // booleans
+                // if statments
         let htmlEs6 = `
             <ul>
                 <li>Name: ${literalName} </li>
@@ -470,8 +480,59 @@ let array = testConcat.indexOf('L');
                 <li>Job: ${literalJob} </li>
                 <li>City: ${literalCity}</li>
                 <li>${2 + 2}</li>
+                <li>${hello()}</li>
+                <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
             </ul>
         `;
           document.body.innerHTML = htmlEs6;
 
+        
 
+
+
+
+// ############### Arrays 
+
+// Creating arrays - recomended
+const numbersArray = [21,2,53,4,45];
+
+// Arrays constructor
+const numbersArrays2 = new Array(22,33,4,4,552,2341);
+
+const fruitArray = ['Apple', 'Banana', 'Orange', 'Pear'];
+
+const mixedArray = [22, 'hello', null, undefined, {a:1, b:2}, new Date()];
+            console.log(mixedArray);
+
+let valArray;
+            console.log(valArray);
+            console.log(numbersArray);
+
+// Checking array length - same as with string
+console.log(mixedArray.length);
+
+// check if something is a array
+valArray = Array.isArray(mixedArray);
+console.log(valArray);
+
+// getting single value
+valArray = mixedArray[4];
+console.log(valArray);
+
+// Inserting into array
+numbersArray[2] = 'test number 100';
+            // arrays are mutable, we can change individual values
+console.log(numbersArray);
+
+// Finding index of a value
+valArray = numbersArray.indexOf('test number 100');
+console.log(valArray);
+
+// Mutating arrays
+    // adding into array - end of array
+numbersArray.push('pushed on the end');
+console.log(numbersArray);
+
+    // ading to the front
+numbersArray.unshift('add to the beggining');
+console.log(numbersArray);
