0b3e286ce6a20ee0f4931f16b7378add9f3636f0
Dawid <dawidgalsino@vp.pl>
2019-09-15 23:22:48 +0200

    create a basic regex form validation

--- 7_1_regex_validation_project/js/app.js
+++ 7_1_regex_validation_project/js/app.js
@@ -16,7 +16,7 @@ function validateName(){
     const regularExpression = /^[a-zA-Z]{2,10}$/;
 
     if(!regularExpression.test(name.value)){
-        console.log(name.value)
+
         name.classList.add('is-invalid')
     }else {
         name.classList.remove('is-invalid')
@@ -24,14 +24,60 @@ function validateName(){
 }
 
 function validateZip(){
+    const zip = document.getElementById('zip');
 
+    // //For polsih format ie 02-703
+    // const regularExpressionZipPl =/^\d{2}-\d{3}$/;
+
+    // //For US format
+    // const regularExpressionZipUSA = /^\d{5}$/
+
+    const regularExpression = /^[0-9]{5}(-[0-9]{4})?$/;
+
+    if(!regularExpression.test(zip.value)){
+        zip.classList.add('is-invalid');
+    } else {
+        zip.classList.remove('is-invalid')
+    }
 }
 
 function validateEmail(){
+    const email = document.getElementById('email');
+    //Expression explanation:
+        //([a-zA-Z0-9_\-\.]+) 
+            //Search for alphanumeric characters both cases [a-zA-Z0-9]
+            //Also for: _ , -, . [_\-\.]
+            //For more than one of those []+
+        //literal at between two groups ()@()
+        //again same pattern in group ([a-zA-Z0-9_\-\.]+)
+        //Literal dot escaped \.
+        //.com and other addr- ([a-zA-Z]{2,5})
 
+    const regularExpression = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+    if(!regularExpression.test(email.value)){
+        email.classList.add('is-invalid');
+    }else {
+        email.classList.remove('is-invalid');
+    }
 }
 
 function validatePhone(){
+    const phone = document.getElementById('phone');
+    //const regularExpression = /^(\+?\d{1,3}-)?\d{3}\-?\d{3}$/;
+    //Accepted formats will be
+        //555555555
+        //555 555 5555 - [-. ]? optional space
+        //555-555-5555 - [-. ]? optional dash separator
+        //555.555.5555 - [-. ]? optional dot seperator
+        //(555)555.5555 - chandles optional () for 3 first digits \(? \)?
 
+    const regularExpression = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+
+    if(!regularExpression.test(phone.value)){
+        phone.classList.add('is-invalid');
+    }else {
+        phone.classList.remove('is-invalid');
+    }
 }
 
